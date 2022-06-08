5e5b9d75fa88111bdde430ac952da72588249948
Ana Paula Leão <paula.leao@gmail.com>
2022-06-08 17:46:48 -0300

    Exercicio 1 - Cálculo da sua Idade

---
+++ Aula2_Conversao/Aula2_Conversao/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Program
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            //1) Faça um programa que pergunte ao usuário ano de nascimento e imprima sua idade.
+
+            int idade;
+            int anoAtual;
+            int anoNasc;
+
+            Console.WriteLine("Vou descobrir sua idade, vamos tentar?");
+            Console.ReadLine();
+
+            Console.WriteLine("Qual o ano atual?\n\n");
+            anoAtual = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Qual o ano do seu nascimento?\n\n");
+            anoNasc = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você tem:\n\n");
+            idade = anoAtual - anoNasc;
+            Console.WriteLine($"{idade} anos");
+
+
+
+        }
+    }
+}
+
+
+
