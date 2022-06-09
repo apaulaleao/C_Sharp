283753e7e251373cae29a8576816866885cbf03d
Ana Paula Leão <paula.leao@gmail.com>
2022-06-08 21:25:14 -0300

    Exercicio 2 - Valores inteiros e soma

--- Aula2_Conversao/Aula2_Conversao/Program.cs
+++ Aula2_Conversao/Aula2_Conversao/Program.cs
@@ -25,6 +25,46 @@ namespace Program
             idade = anoAtual - anoNasc;
             Console.WriteLine($"{idade} anos");
 
+            // 2) Escreva um programa que leia 10 valores inteiros e ao final exiba a soma dos números
+
+
+            Console.WriteLine("Escreva um programa que leia 10 valores inteiros e ao final exiba a soma dos números");
+            Console.ReadLine();
+
+            int a, b, c, d, e, f, g, h, i, j;
+            int soma;
+
+            Console.WriteLine("Digite o 1 num");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 2 num");
+            b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 3 num");
+            c = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 4 num");
+            d = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 5 num");
+            e = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 6 num");
+            f = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 7 num");
+            g = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 8 num");
+            h = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 9 num");
+            i = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o 10 num");
+            j = Convert.ToInt16(Console.ReadLine());
+
+
+
+            soma = (a + b + c + d + e + f + g + h + i + j);
+            //soma=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"A soma dos números é {soma}. =)");
+            
+
+
+
+
 
 
         }
